8fa0d93b9e9f659b0ca0815016571e2ace11ecfb
Sunil Ramchandani <ramch101@mail.chapman.edu>
2015-12-06 18:38:44 -0800

    Work in Progress

---
+++ generate_output.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# This script is used to comple and generate CSV file for julia and mandelbrot programs.
+
+# Display the current directory
+SCRIPT=$(readlink -f "$0")
+SCRIPTPATH=$(dirname "$SCRIPT")
+echo "The prorgram is running from following directory: " $SCRIPTPATH
+
+# Compile the programs
+(cd C-JuliaSets && make julia)
+(cd Cpp-Mandelbrot && make mandelbrot)
+
+# Run the programs
+(cd C-JuliaSets && ./julia -2 2 -2 2 100 100 -1.037 0.17 > juliaset.csv)
+(cd Cpp-Mandelbrot && ./mandelbrot -2 2 -2 2 100 100 > mandelbrot.csv)
